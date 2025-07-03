24bc6d28b2e2da995a8483e200193ef228a47c3f
Flávio Félix <fau_felix30@hotmail.com>
2025-07-02 21:36:57 -0300

    feat: Components Logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-	import '../app.css';
-	
-	let { children } = $props();
+  import "../app.css";
+  import Logo from "../components/template/Logo.svelte";
+
+  let { children } = $props();
 </script>
 
-{@render children()}
+<div
+  class="flex flex-col justify-center items-center gap-5 bg-black text-white min-h-screen"
+>
+  <Logo />
+  {@render children()}
+</div>
