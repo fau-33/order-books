efee71152e383a8164862057ea1d4e1b3c742144
Flávio Félix <fau_felix30@hotmail.com>
2025-07-04 15:42:16 -0300

    feat: Done components page the header

---
+++ src/components/template/Pagina.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Logo from "./Logo.svelte";
+</script>
+
+<div class="flex flex-col min-h-screen bg-black text-white">
+  <Logo />
+  <main>
+    <slot></slot>
+  </main>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
   import "../app.css";
-  import Logo from "../components/template/Logo.svelte";
+  import Pagina from "../components/template/Pagina.svelte";
 
   let { children } = $props();
 </script>
 
-<div
-  class="flex flex-col justify-center items-center gap-5 bg-black text-white min-h-screen"
->
-  <Logo />
+<Pagina>
   {@render children()}
-</div>
+</Pagina>
